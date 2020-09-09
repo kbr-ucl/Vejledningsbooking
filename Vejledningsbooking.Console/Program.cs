baa6b4dde64184a3cfff98a3fed0d3cec5b464a5
Kaj Bromose <kbr@ucl.dk>
2020-09-09 17:27:18 +0200

    Add project files.

---
+++ Vejledningsbooking.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Vejledningsbooking.ConsoleApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
